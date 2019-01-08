0852299e481e41badcbe4c81be487da3f1968aa2
psknayak <psknayak@gmail.com>
2019-01-08 15:32:02 +0530

    Create a basic reply bot

--- code.py
+++ code.py
@@ -12,4 +12,11 @@ client = discord.Client()
 async def on_ready(): #called one time when bot is connected to server
     print(f"Logged in as {client.user}")
 
+@client.event
+async def on_message(message):
+    print(f"{message.channel}:{message.author}:{message.author.name}:{message.content}")
+
+    if "hi there" in message.content.lower():
+        await message.channel.send("Hello")
+        
 client.run("NTMyMTI3NTQ2MjI2MTE0NTYz.DxX-oQ.zdHB0JS9Mk5dPDsHRBSeZoD4gyk")
